448293ecfe244c962f57e41ae4a4a829b6fac4f6
bryamOzl <bryan95_19@hotmail.com>
2021-07-22 13:06:46 -0500

    Ultima Version

--- src/main/java/ec/edu/ups/austro/negocio/MovimientoON.java
+++ src/main/java/ec/edu/ups/austro/negocio/MovimientoON.java
@@ -11,14 +11,13 @@ import ec.edu.ups.austro.modelo.Cuenta;
 import ec.edu.ups.austro.modelo.Movimiento;
 
 @Stateless
-public class MovimientoON implements MovimientoONLocal{
+public class MovimientoON implements MovimientoONLocal {
 
 	@Inject
 	private MovimientoDAO daoMovimiento;
 
 	@Inject
 	private CuentaDAO cuentaDAO;
-	
 
 	private Cuenta cuenta = new Cuenta();
 
@@ -39,17 +38,32 @@ public class MovimientoON implements MovimientoONLocal{
 		return id;
 	}
 
+	/*
+	 * public Cuenta cuenta(String nCuenta) throws SQLException { cuenta =
+	 * cuentaDAO.buscarCuenta(nCuenta); System.out.println(cuenta); return cuenta; }
+	 */
 
-	public Cuenta cuenta(String nCuenta) throws SQLException {
-		cuenta = cuentaDAO.buscarCuenta(nCuenta);
-		System.out.println(cuenta);
+	public Cuenta cuenta(String nCuenta) {
+		// System.out.println(cuenta);
+		// return cuenta;
+		cuenta = new Cuenta();
+		try {
+			cuenta = cuentaDAO.buscarCuenta(nCuenta);
+			System.out.println(cuenta);
+		} catch (SQLException e) {
+			// TODO: handle exception
+			cuenta.setEntidadFinanciera("NO EXISTE");
+			cuenta.setCuentaId(0);
+			cuenta.setNumeroCuenta("0000000001");
+			cuenta.setSaldo(0.0);
+			System.out.println(cuenta);
+		}
 		return cuenta;
 	}
-	
+
 	public boolean actualizarCuenta(Cuenta cuenta) throws SQLException {
 		cuentaDAO.update(cuenta);
 		return true;
 	}
 
-	
 }
